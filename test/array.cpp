59331f376cce08a6a618a951395d3a2c83ae1a40
Pavel Turapin <paveltrpn@integra-s.com>
2023-12-29 17:47:53 +0400

    add dummy test.cpp

--- test/array.cpp
+++ test/array.cpp
@@ -1,8 +1,4 @@
 
-#define BOOST_TEST_DYN_LINK
-
-#define BOOST_TEST_MODULE test_unit
-
 #include "../src/array.h"
 
 #include <boost/test/unit_test.hpp>
---
+++ test/test.cpp
@@ -0,0 +1,14 @@
+
+
+#define BOOST_TEST_DYN_LINK
+
+#define BOOST_TEST_MODULE test_unit
+
+#include <boost/test/unit_test.hpp>
+
+BOOST_AUTO_TEST_SUITE(dummy)
+
+BOOST_AUTO_TEST_CASE(case_creation) {
+}
+
+BOOST_AUTO_TEST_SUITE_END()
