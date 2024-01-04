5c0159cb94968ee0356cd27f23c3b602873d3003
Pavel Turapin <paveltrpn@integra-s.com>
2024-01-05 02:51:31 +0400

    list is working

--- src/list.cpp
+++ src/list.cpp
@@ -7,14 +7,90 @@ export module toy_stl.list;
 
 namespace toy {
 
-struct node_base {
-        std::shared_ptr<node_base> next_;
-        std::shared_ptr<node_base> prev_;
+struct list_node_base {
+        void swap(list_node_base& x, list_node_base& y) noexcept {
+            if (x.next_ != &x) {
+                if (y.next_ != &y) {
+                    // Both x and y are not empty.
+                    std::swap(x.next_, y.next_);
+                    std::swap(x.prev_, y.prev_);
+                    x.next_->prev_ = x.prev_->next_ = &x;
+                    y.next_->prev_ = y.prev_->next_ = &y;
+                } else {
+                    // x is not empty, y is empty.
+                    y.next_ = x.next_;
+                    y.prev_ = x.prev_;
+                    y.next_->prev_ = y.prev_->next_ = &y;
+                    x.next_ = x.prev_ = &x;
+                }
+            } else if (y.next_ != &y) {
+                // x is empty, y is not empty.
+                x.next_ = y.next_;
+                x.prev_ = y.prev_;
+                x.next_->prev_ = x.prev_->next_ = &x;
+                y.next_ = y.prev_ = &y;
+            }
+        }
+
+        void transfer(list_node_base* const first, list_node_base* const last) {
+            // assert(first != last);
+            if (this != last) {
+                // Remove [first, last) from its old position.
+                last->prev_->next_ = this;
+                first->prev_->next_ = last;
+                this->prev_->next_ = first;
+
+                // Splice [first, last) into its new position.
+                list_node_base* const tmp = this->prev_;
+                this->prev_ = last->prev_;
+                last->prev_ = first->prev_;
+                first->prev_ = tmp;
+            }
+        }
+
+        void reverse() {
+            list_node_base* tmp = this;
+            do {
+                std::swap(tmp->next_, tmp->prev_);
+
+                // Old next node is now prev.
+                tmp = tmp->prev_;
+            } while (tmp != this);
+        }
+
+        void hook(list_node_base* const position) {
+            this->next_ = position;
+            this->prev_ = position->prev_;
+            position->prev_->next_ = this;
+            position->prev_ = this;
+        }
+
+        void unhook() {
+            list_node_base* const next_node = this->next_;
+            list_node_base* const prev_node = this->prev_;
+            prev_node->next_ = next_node;
+            next_node->prev_ = prev_node;
+        }
+
+        list_node_base* next_;
+        list_node_base* prev_;
+};
+
+struct list_node_header : public list_node_base {
+        list_node_header() {
+            init();
+        }
+
+        void init() noexcept {
+            this->next_ = this->prev_ = this;
+        }
 };
 
 template <typename T>
-struct node : public node_base {
-        node(const T& data) : data_{ data } {
+struct list_node : public list_node_base {
+        list_node() = default;
+
+        list_node(const T& data) : data_{ data } {
         }
 
         T* valptr() {
@@ -25,80 +101,80 @@ struct node : public node_base {
             return std::addressof(data_);
         }
 
-        T operator()() {
-            return data_;
-        }
-
     private:
-        T data_{};
+        T data_;
 };
 
 template <typename T>
 struct list_iterator {
-        using self_ = list_iterator<T>;
-        using node_ = node<T>;
+        using self = list_iterator<T>;
+        using node_type = list_node<T>;
 
         using value_type = T;
+        using reference = T&;
+        using pointer = T*;
         using difference_type = std::ptrdiff_t;
-        using reference = node_&;
-        using pointer = node_*;
         using iterator_category = std::bidirectional_iterator_tag;
-    
-        list_iterator(): valuePtr_{} {};
-        explicit list_iterator(node_base* ptr) : valuePtr_{ ptr } {};
+
+        list_iterator() = default;
+
+        explicit list_iterator(list_node_base* ptr): node_ptr_{ptr} {};
 
         reference operator*() {
-            return *static_cast<node_*>(valuePtr_)->valptr();
+            return *static_cast<node_type*>(node_ptr_)->valptr();
         };
 
         pointer operator->() {
-            return std::addressof(static_cast<node_*>(valuePtr_)->valptr());
+            return std::addressof(static_cast<node_type*>(node_ptr_)->valptr());
         }
 
         // pre-increment
-        self_& operator++() {
-            valuePtr_ = valuePtr_->next_;
+        self& operator++() {
+            node_ptr_ = node_ptr_->next_;
             return *this;
         }
 
         // post-increment
-        self_ operator++(int) {
+        self operator++(int) {
             auto tmp{ *this };
             ++(*this);
             return tmp;
         }
 
         // pre-decrement
-        self_& operator--() {
-            valuePtr_ = valuePtr_->prev_;
+        self& operator--() {
+            node_ptr_ = node_ptr_->prev_;
             return *this;
         }
 
         // post-decrement
-        self_ operator--(int) {
+        self operator--(int) {
             auto tmp{ *this };
             --(*this);
             return tmp;
         }
 
-        friend bool operator==(const self_& a, const self_& b) {
-            return a.valuePtr_ == b.valuePtr_;
+        friend bool operator==(const self& a, const self& b) {
+            return a.node_ptr_ == b.node_ptr_;
         };
 
-        friend bool operator!=(const self_& a, const self_& b) {
+        friend bool operator!=(const self& a, const self& b) {
             return !(a == b);
         };
 
-    private:
-        std::shared_ptr<node_base> valuePtr_;
+        list_node_base* node_ptr_{};
 };
 
 export template <typename T>
 struct list {
         using iterator = list_iterator<T>;
         using value_type = T;
+        using reference = T&;
+        using const_reference = const T&;
 
-        explicit list() = default;
+        using node_type = list_node<T>;
+
+        list() = default;
 
         list(const list& rhs) = delete;
         list(list&& rhs) = delete;
@@ -107,25 +183,62 @@ struct list {
         list& operator=(list&& rhs) = delete;
 
         iterator begin() {
-            return iterator{ front_ };
+            return iterator{ head_.next_ };
         }
 
         iterator end() {
-            return iterator{};
+            return iterator{ &head_ };
         }
 
-        void push(const T& rhs) {
-            ++size_;
+        void insert(iterator position, const value_type& rhs) {
+            auto* next = new node_type{ rhs };
+            next->hook(position.node_ptr_);
+            size_++;
         }
 
-        T& pop() {
+        void erase(iterator position) {
+            size_--;
+            position.node_ptr_->unhook();
+            auto* n = static_cast<node_type*>(position.node_ptr_);
+            //delete n->valptr();
+            delete n;
+        }
+
+        reference front() {
+            return *begin();
+        }
+
+        reference back() {
+            iterator tmp = end();
+            --tmp;
+            return *tmp;
+        }
+
+        void push_back(const value_type& rhs) {
+            insert(this->end(), rhs);
+        }
+
+        void push_front(const value_type& rhs) {
+            insert(this->begin(), rhs);
+        }
+
+        void pop_back() {
+            erase(iterator{ head_.prev_ });
+        }
+
+        void pop_front() {
+            erase(begin());
+        }
+
+        [[nodiscard]]
+        size_t size() const noexcept {
+            return size_;
         }
 
     private:
-        std::shared_ptr<node<T>> front_{};
-        std::shared_ptr<node<T>> back_{};
+        size_t size_{ 0 };
 
-        size_t size_{};
+        list_node_header head_;
 };
 
 }  // namespace toy
--- test/list.cpp
+++ test/list.cpp
@@ -15,29 +15,77 @@ BOOST_AUTO_TEST_SUITE(list)
 BOOST_AUTO_TEST_CASE(case_construct_push_pop) {
     toy::list<std::string> first{};
 
-    first.push("lisa");
-    BOOST_CHECK_EQUAL(first.pop(), "lisa");
+    first.push_back("lisa");
+    BOOST_CHECK_EQUAL(first.back(), "lisa");
+    first.push_back("bob");
+    BOOST_CHECK_EQUAL(first.back(), "bob");
+    first.push_back("woody");
+    BOOST_CHECK_EQUAL(first.back(), "woody");
+    first.push_back("martin");
+    BOOST_CHECK_EQUAL(first.back(), "martin");
 
-    first.push("bob");
-    BOOST_CHECK_EQUAL(first.pop(), "bob");
+    BOOST_CHECK(first.size() == 4);
+    
+    auto str = first.back();
+    first.pop_back();
+    BOOST_CHECK_EQUAL(str, "martin");
 
-    first.push("woody");
-    BOOST_CHECK_EQUAL(first.pop(), "woody");
+    str = first.back();
+    first.pop_back();
+    BOOST_CHECK_EQUAL(str, "woody");
+    
+    str = first.back();
+    first.pop_back();
+    BOOST_CHECK_EQUAL(str, "bob");
+    
+    str = first.back();
+    first.pop_back();
+    BOOST_CHECK_EQUAL(str, "lisa");
 
-    first.push("martin");
-    BOOST_CHECK_EQUAL(first.pop(), "martin");
+    BOOST_CHECK(first.size() == 0);
+
+    first.push_front("lisa");
+    BOOST_CHECK_EQUAL(first.front(), "lisa");
+    first.push_front("bob");
+    BOOST_CHECK_EQUAL(first.front(), "bob");
+    first.push_front("woody");
+    BOOST_CHECK_EQUAL(first.front(), "woody");
+    first.push_front("martin");
+    BOOST_CHECK_EQUAL(first.front(), "martin");
+
+    BOOST_CHECK(first.size() == 4);
+    
+    str = first.front();
+    first.pop_front();
+    BOOST_CHECK_EQUAL(str, "martin");
+
+    str = first.front();
+    first.pop_front();
+    BOOST_CHECK_EQUAL(str, "woody");
+    
+    str = first.front();
+    first.pop_front();
+    BOOST_CHECK_EQUAL(str, "bob");
+    
+    str = first.front();
+    first.pop_front();
+    BOOST_CHECK_EQUAL(str, "lisa");
+
+    BOOST_CHECK(first.size() == 0);
 }
 
 BOOST_AUTO_TEST_CASE(case_range_for) {
-    toy::list<std::string> first{};
+    toy::list<std::string> first;
     std::vector<std::string> tst{ "lisa", "bob", "woody", "martin" };
 
     for (auto &elem : tst) {
-        first.push(elem);
+        first.push_back(elem);
     }
 
-    for (auto elem : first) {
+    for (size_t i = 0; auto &elem : first) {
+        BOOST_CHECK_EQUAL(elem, tst[i]);
+        ++i;
     }
 }
 
-BOOST_AUTO_TEST_SUITE_END()  // list
+BOOST_AUTO_TEST_SUITE_END()  // list:
