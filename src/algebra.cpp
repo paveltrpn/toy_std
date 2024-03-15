ff9e8dc173d46b3fa02caa2e029c3dd231c28213
Pavel Turapin <paveltrpn@mail.ru>
2024-03-15 12:07:20 +0400

    move algebra to tiny-render

--- src/algebra.cpp
+++
@@ -1,7 +0,0 @@
-module;
-
-export module toy_std.algebra;
-
-export import :matrix;
-export import :quaternion;
-export import :vector;
--- src/toy_std.cpp
+++ src/toy_std.cpp
@@ -9,7 +9,6 @@ export import toy_std.raw_list;
 export import toy_std.vector;
 export import toy_std.raw_vector;
 export import toy_std.stack;
-export import toy_std.algebra;
 export import toy_std.iterator_traits;
 export import toy_std.algorithm;
 export import toy_std.optional;
